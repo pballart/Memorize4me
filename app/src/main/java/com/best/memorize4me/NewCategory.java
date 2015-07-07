5e9dc32d530a65aa2785f7b6cc4032de7285efa5
Laia Freixas <laiafreixas9@gmail.com>
2015-07-07 09:44:27 +0200

    Layout for NewCategory activity

---
+++ app/src/main/java/com/best/memorize4me/NewCategory.java
@@ -0,0 +1,43 @@
+package com.best.memorize4me;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.best.memorize4me.db.interfaces.AppInterface;
+
+
+public class NewCategory extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_category);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_category, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.saveCategory){
+           //TODO CALL METHOD CREATE CATEGORY
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
