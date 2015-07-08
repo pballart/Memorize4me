6df8cb0de8b8a14149cd5e13ec3e96ac5b936cf0
konstantin.bogdanov <konstantin.bogdanov@corp.mail.ru>
2015-07-08 12:44:10 +0300

    contact name

--- app/src/main/java/com/best/memorize4me/db/model/Contact.java
+++ app/src/main/java/com/best/memorize4me/db/model/Contact.java
@@ -4,8 +4,7 @@ package com.best.memorize4me.db.model;
  * Created by konstantin.bogdanov on 06.07.2015.
  */
 public class Contact {
-    public String firstName;
-    public String lastName;
+    public String name;
     public String phoneNumber;
     public String email;
 }
