e698b19a2e01811647009478804dc361205d2704
Laia Freixas <laiafreixas9@gmail.com>
2015-07-09 13:15:51 +0200

    changed date format and description view in preview search item

--- app/src/main/java/com/best/memorize4me/SearchItemPreviewActivity.java
+++ app/src/main/java/com/best/memorize4me/SearchItemPreviewActivity.java
@@ -18,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.best.memorize4me.db.model.SearchItem;
+import com.best.memorize4me.util.DateUtils;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -50,19 +51,22 @@ public class SearchItemPreviewActivity extends ActionBarActivity {
         TextView date = (TextView) findViewById(R.id.dateTextView);
         TextView price = (TextView) findViewById(R.id.priceTextView);
         TextView rate = (TextView) findViewById(R.id.rateTextView);
-        //TextView multilineDescription = (TextView) findViewById(R.id.multilineDescriptionTextView);
+        TextView multilineDescription = (TextView) findViewById(R.id.multilineDescriptionTextView);
         TextView contactName = (TextView) findViewById(R.id.contactNameTextView);
         TextView contactPhoneNumber = (TextView) findViewById(R.id.contactPhoneNumberTextView);
         TextView contactEmail = (TextView) findViewById(R.id.contactEmailTextView);
         imageView = (ImageView) findViewById(R.id.imageView);
 
         title.setText(searchItem.title);
-        date.setText(String.valueOf(searchItem.date));
+        date.setText(DateUtils.dateToString(searchItem.date));
+
+
         price.setText(String.valueOf(searchItem.price));
         rate.setText("Rate: " + String.valueOf(searchItem.rate));
         contactName.setText("Contact: " + searchItem.contact.name);
         contactPhoneNumber.setText("Tel: " + searchItem.contact.phoneNumber);
         contactEmail.setText(searchItem.contact.email);
+        multilineDescription.setText (searchItem.description);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
